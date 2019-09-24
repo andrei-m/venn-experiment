c19834ad68bd8ee4efc1af1f25d0ba6dc985d846
Andrei Mackenzie <andrei@clypd.com>
2019-09-24 19:59:50 -0400

    basic venn diagram

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as venn from 'venn.js';
+import * as d3 from 'd3';
 
 class Diagram extends React.Component {
+  componentDidMount() {
+	var sets = [ {sets: ['A'], size: 12},
+				 {sets: ['B'], size: 12},
+				 {sets: ['A','B'], size: 6}];
+
+	var chart = venn.VennDiagram();
+	d3.select(".venn").datum(sets).call(chart);
+  }
+
   render() {
-    return (<div>Hello</div>);
+    return (<div className="venn"></div>);
   }
 }
 
