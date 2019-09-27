04543623a1716ba8afd08b8c889a6c929d8781ad
Andrei Mackenzie <andrei@clypd.com>
2019-09-27 17:46:05 -0400

    Example with tooltip

--- src/index.js
+++ src/index.js
@@ -21,19 +21,39 @@ class TwoElement extends React.Component {
 				 {sets: ['DisjunctionA','DisjunctionB'], size: 8}]; // conjunction
 
 	var chart = venn.VennDiagram();
-	d3.select(".TwoElement").datum(sets).call(chart);
+    var div = d3.select(".TwoElement");
+	div.datum(sets).call(chart);
 
-    d3.selectAll(".TwoElement .venn-circle")
+    var tooltip = d3.select("body").append("div")
+       .attr("class", "venntooltip");
+
+    div.selectAll("g")
       .on("mouseover", function(d, i) {
-        var node = d3.select(this).transition();
-        node.select("text").style("font-weight", "100")
-                           .style("font-size", "36px");
-      })
-      .on("mouseout", function(d, i) {
-        var node = d3.select(this).transition();
-        node.select("text").style("font-weight", "100")
-                           .style("font-size", "24px");
-      });
+		// sort all the areas relative to the current item
+        venn.sortAreas(div, d);
+        // Display a tooltip with the current size
+        tooltip.transition().duration(400).style("opacity", .9);
+        tooltip.text(d.size + " users");
+
+        // highlight the current path
+        var selection = d3.select(this).transition("tooltip").duration(400);
+        selection.select("path")
+        .style("stroke-width", 3)
+        .style("fill-opacity", d.sets.length == 1 ? .4 : .1)
+        .style("stroke-opacity", 1);
+    })
+	.on("mousemove", function() {
+        tooltip.style("left", (d3.event.pageX) + "px")
+               .style("top", (d3.event.pageY - 28) + "px");
+    })
+    .on("mouseout", function(d, i) {
+        tooltip.transition().duration(400).style("opacity", 0);
+        var selection = d3.select(this).transition("tooltip").duration(400);
+        selection.select("path")
+            .style("stroke-width", 0)
+            .style("fill-opacity", d.sets.length == 1 ? .25 : .0)
+            .style("stroke-opacity", 0);
+    });
   }
 
   render() {
