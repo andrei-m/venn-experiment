9778543eff7d5174fdcfc4928fbfac1d821a820c
Andrei Mackenzie <andrei@clypd.com>
2019-09-27 17:36:55 -0400

    An additional example, this time with 3 elements

--- src/index.js
+++ src/index.js
@@ -3,19 +3,61 @@ import ReactDOM from 'react-dom';
 import * as venn from 'venn.js';
 import * as d3 from 'd3';
 
-class Diagram extends React.Component {
-  componentDidMount() {
-	var sets = [ {sets: ['A'], size: 12},
-				 {sets: ['B'], size: 20},
-				 {sets: ['A','B'], size: 8}];
-
-	var chart = venn.VennDiagram();
-	d3.select(".venn").datum(sets).call(chart);
-  }
-
+class Examples extends React.Component {
   render() {
-    return (<div className="venn"></div>);
+    return (
+      <div>
+        <TwoElement />
+        <ThreeElement />
+      </div>
+    );
   }
 }
 
-ReactDOM.render(<Diagram />, document.getElementById('root'));
+class TwoElement extends React.Component {
+  componentDidMount() {
+	var sets = [ {sets: ['DisjunctionA'], size: 12}, // disjunction
+				 {sets: ['DisjunctionB'], size: 20}, // disjunction
+				 {sets: ['DisjunctionA','DisjunctionB'], size: 8}]; // conjunction
+
+	var chart = venn.VennDiagram();
+	d3.select(".TwoElement").datum(sets).call(chart);
+
+    d3.selectAll(".TwoElement .venn-circle")
+      .on("mouseover", function(d, i) {
+        var node = d3.select(this).transition();
+        node.select("text").style("font-weight", "100")
+                           .style("font-size", "36px");
+      })
+      .on("mouseout", function(d, i) {
+        var node = d3.select(this).transition();
+        node.select("text").style("font-weight", "100")
+                           .style("font-size", "24px");
+      });
+  }
+
+  render() {
+    return (<div className="TwoElement"></div>);
+  }
+}
+
+class ThreeElement extends React.Component {
+  componentDidMount() {
+	var sets = [ {sets: ['DisjunctionA'], size: 12}, // disjunction
+				 {sets: ['DisjunctionB'], size: 20}, // disjunction
+				 {sets: ['DisjunctionC'], size: 25}, // disjunction
+				 {sets: ['DisjunctionA','DisjunctionB'], size: 10}, // conjunction
+				 {sets: ['DisjunctionA','DisjunctionC'], size: 10}, // conjunction
+				 {sets: ['DisjunctionB','DisjunctionC'], size: 10}, // conjunction
+				 {sets: ['DisjunctionA','DisjunctionB','DisjunctionC'], size: 5}]; // conjunction
+
+	var chart = venn.VennDiagram();
+	d3.select(".ThreeElement").datum(sets).call(chart);
+  }
+
+  render() {
+    return (<div className="ThreeElement"></div>);
+  }
+}
+
+ReactDOM.render(<Examples />, document.getElementById('root'));
